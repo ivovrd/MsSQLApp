19c6be73f05bbffb7f4e616006ba9f69328d5524
ivovrd <ivo.vrdoljak91@gmail.com>
2015-08-26 19:22:03 +0200

    Minor changes

---
+++ app/src/main/java/com/example/ivo/mssqlapp/Ovlastenik.java
@@ -0,0 +1,14 @@
+package com.example.ivo.mssqlapp;
+
+/**
+ * Created by Ivo on 26.8.2015..
+ */
+public class Ovlastenik {
+    public int Id;
+    public String Naziv;
+
+    public Ovlastenik(int id, String naziv){
+        this.Id = id;
+        this.Naziv = naziv;
+    }
+}
--- app/src/main/java/com/example/ivo/mssqlapp/ThirdFragment.java
+++ app/src/main/java/com/example/ivo/mssqlapp/ThirdFragment.java
@@ -2,11 +2,13 @@ package com.example.ivo.mssqlapp;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -32,7 +34,10 @@ public class ThirdFragment extends Fragment implements View.OnClickListener{
     private Connection connect;
     private Statement statement;
     private Spinner spinner;
-    private ArrayList<String> ovlastenici;
+    private EditText daysCount, workDaysCount;
+    private ArrayList<Ovlastenik> ovlastenici;
+    private int ovlastenikPickedIndex;
+    private long from, to, diff = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,7 +49,8 @@ public class ThirdFragment extends Fragment implements View.OnClickListener{
             statement = connect.createStatement();
             ResultSet resultSet = statement.executeQuery("select Id, Naziv from Sifrarnici.Partner");
             while (resultSet.next()){
-                ovlastenici.add(resultSet.getString("Naziv"));
+                Ovlastenik ovlastenik = new Ovlastenik(resultSet.getInt("Id"), resultSet.getString("Naziv"));
+                ovlastenici.add(ovlastenik);
             }
 
         }catch (SQLException e){
@@ -56,17 +62,42 @@ public class ThirdFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.third_fragment, container, false);
-
+        final View view =  inflater.inflate(R.layout.third_fragment, container, false);
         spinner = (Spinner)view.findViewById(R.id.spinner);
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_dropdown_item, ovlastenici);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
         dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
         dateFrom = (EditText)view.findViewById(R.id.editDateFrom);
         dateTo = (EditText)view.findViewById(R.id.editDateTo);
+        daysCount = (EditText)view.findViewById(R.id.daysCount);
+        workDaysCount = (EditText)view.findViewById(R.id.workDaysCount);
+
+        fillSpinner();
         setDateFields();
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ovlastenikPickedIndex = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        workDaysCount.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    int daysWork = Integer.valueOf(workDaysCount.getText().toString());
+                    int days = (int)diff;
+                    if(days - daysWork < 0){
+                        Snackbar.make(view, "Višak radnih dana!!!", Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
         return view;
     }
 
@@ -89,6 +120,10 @@ public class ThirdFragment extends Fragment implements View.OnClickListener{
                 Calendar newDate = Calendar.getInstance();
                 newDate.set(year, monthOfYear, dayOfMonth);
                 dateFrom.setText(dateFormat.format(newDate.getTime()));
+                from = newDate.getTimeInMillis();
+                diff = (to - from)/(24*60*60*1000);
+                if(diff >= 0)
+                    daysCount.setText(String.valueOf(diff));
             }
         }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
 
@@ -98,7 +133,23 @@ public class ThirdFragment extends Fragment implements View.OnClickListener{
                 Calendar newDate = Calendar.getInstance();
                 newDate.set(year, monthOfYear, dayOfMonth);
                 dateTo.setText(dateFormat.format(newDate.getTime()));
+                to = newDate.getTimeInMillis();
+                diff = (to - from)/(24*60*60*1000);
+                if(diff >= 0)
+                    daysCount.setText(String.valueOf(diff));
             }
         }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    private void fillSpinner(){
+        ArrayList<String> labels = new ArrayList<>();
+
+        for(int i = 0; i < ovlastenici.size(); i++){
+            labels.add(ovlastenici.get(i).Naziv);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_dropdown_item, labels);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
